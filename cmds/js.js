d69d497b66ff61e1cd85bee942fbe77bd6177cc9
Meepen <meepdarknessmeep@gmail.com>
2020-02-24 17:43:20 -0500

    make js have require

--- cmds/js.js
+++ cmds/js.js
@@ -3,14 +3,14 @@ const {ArgumentError} = require("../errors");
 
 module.exports.Command = class JSCommand {
     constructor(bot, msg, args) {
-        let fn = new Function("bot", "msg", args[0]);
+        let fn = new Function("bot", "msg", "require", args[0]);
 
 	if (!fn) {
 		msg.reply("Couldn't create function");
 	}
 
 	try {
-		let res = fn(bot, msg);
+		let res = fn(bot, msg, require);
 
 		if (res) {
 			msg.reply("result: " + res.toString());
