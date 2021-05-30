4bf5703b9feb37332831ae3fa780e4c7588269e0
add___123 <45610714+itsmeaddof123@users.noreply.github.com>
2021-05-30 18:28:33 -0500

    Fix this this

--- libs/servers/updater.js
+++ libs/servers/updater.js
@@ -74,7 +74,7 @@ module.exports.Updater = class Updater {
 					    let last_date = new Date(Date.UTC(last_split[0], last_split[1] - 1, last_split[2], last_split[3], last_split[4], last_split[5]))
 					    last_date.setTime(this.getTime() + (20 * 60 * 60 * 1000))
 					    if (last_date <= now) {
-						let ping_channel = await this.client.channels.fetch("846886760386658305");
+						let ping_channel = await this.bot.client.channels.fetch("846886760386658305");
 						ping_channel.send("Hey <@&846572582702546984>, feel free to join the server if you're available.");
 						this.bot.db.query("INSERT INTO pings (ping, last) VALUES ('late_joiners', NOW()) ON DUPLICATE KEY UPDATE last = NOW();");
 					    }
