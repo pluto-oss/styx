778e47c6dc31a982f7b747ad9b032d0b4e7a1af2
add___123 <45610714+itsmeaddof123@users.noreply.github.com>
2021-05-30 18:07:18 -0500

    Finish populate command "hypothetically"

--- cmds/populate.js
+++ cmds/populate.js
@@ -4,12 +4,16 @@ module.exports.Command = class PopulateCommand {
             if (err) throw err;
           
             let last = ret[0].last
-            // let now = // current time
-            // if now - last is greater than 20 hours then
+            let now = new Date()
+            let last_split = last.split(/[- :]/)
+            let last_date = new Date(Date.UTC(last_split[0], last_split[1] - 1, last_split[2], last_split[3], last_split[4], last_split[5]))
+            last_date.setTime(this.getTime() + (20 * 60 * 60 * 1000))
+            if (last_date <= now) {
                 msg.channel.send("Hey <@&846572582702546984>, feel free to join the server if you're available.");
-                bot.db.query("INSERT INTO pings (ping, last) VALUES ('early_joiners', NOW()) ON DUPLICATE KEY SET last = VALUE(last);
-            // else
+                bot.db.query("INSERT INTO pings (ping, last) VALUES ('early_joiners', NOW());");
+            } else {
                 msg.channel.send("The last role ping was too recent. Try again later.");
+            }
         });
     }
 
